cbd210d32446fb38fd1febeb4e4f10106e24569b
paulosanttana <paulorobsantana@gmail.com>
2020-07-20 23:09:01 -0300

    Updates

---
+++ precache-manifest.96f04149069d7ce320635302e93ee556.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d241004f531366677fe661d1f5650a74",
+    "url": "/em-planejados/index.html"
+  },
+  {
+    "revision": "a8c36c4be0da3f4654a2",
+    "url": "/em-planejados/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "004e18c0017b1b0acb89",
+    "url": "/em-planejados/static/css/main.c3bf1d7f.chunk.css"
+  },
+  {
+    "revision": "a8c36c4be0da3f4654a2",
+    "url": "/em-planejados/static/js/2.9f871da6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/em-planejados/static/js/2.9f871da6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004e18c0017b1b0acb89",
+    "url": "/em-planejados/static/js/main.3e2d9b22.chunk.js"
+  },
+  {
+    "revision": "04d3a1edcf119e66d52f",
+    "url": "/em-planejados/static/js/runtime-main.9dd0d148.js"
+  }
+]);
